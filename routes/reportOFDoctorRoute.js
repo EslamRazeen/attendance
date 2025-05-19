38c181689da75928de1944f10baf144b6f9770ae
Eslam Razeen <eslamaymrazeen2003@gamil.com>
2025-05-19 15:18:24 +0300

    change get method of report doctor and staff to post

--- routes/reportOFDoctorRoute.js
+++ routes/reportOFDoctorRoute.js
@@ -7,7 +7,7 @@ const authController = require("../controllers/authController");
 
 router
   .route("/")
-  .get(
+  .post(
     authController.protect,
     reportOFDoctorAttendancesController.attendanceReport
   );
--- routes/reportStaffRoute.js
+++ routes/reportStaffRoute.js
@@ -5,6 +5,6 @@ const router = express.Router();
 const reportStaff = require("../controllers/reportSfaffController");
 const authController = require("../controllers/authController");
 
-router.route("/").get(authController.protect, reportStaff.getAttendanceByDay);
+router.route("/").post(authController.protect, reportStaff.getAttendanceByDay);
 
 module.exports = router;
